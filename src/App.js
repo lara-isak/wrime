314da127ca9721d79df2a5d703b1172d47583bb6
Lara Isak <luckasti.larry@gmail.com>
2021-06-15 23:13:42 +0200

    ✨ retrieve and display the geo coords of our device
    get our latitude and longitude using navigator.geolocation and use setLat & setLon to set our latitude and longitude states

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import React, { useEffect, useState } from 'react';
 
+// useState & useEffect are Hooks. With Hooks we can use state and other React features without writing a class
 
 function App() {
+  // 2 states, 1 for the latitude and 1 for the longitude
+  // these 2 states will be used to store the latitude and the longitude of our current location which will be used in the API call to access current weather data
+  const [lat, setLat] = useState([]);
+  const [lon, setLon] = useState([]);
+
+  // goal of the useEffect function is to load the functions inside it when app loads and reloads
+  useEffect(() => {
+    // navigator.geolocation is a property of the Navigator object
+    // navigator.geolocation obtains the Geolocation object which is able to obtain the position of the device using the getCurrentPosition method
+    navigator.geolocation.getCurrentPosition(function(position) {
+      setLat(position.coords.latitude);
+      setLon(position.coords.longitude);
+    });
+  });
+
   return (
     <div className="App">
-
+      <p>Your latitude is {lat}</p>
+      <p>Your longitude is {lon}</p>
     </div>
   );
 }
