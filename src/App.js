add8f61b296cb8c771f4ed8d099c9c533f25b991
Lara Isak <luckasti.larry@gmail.com>
2021-06-21 18:28:18 +0200

    ♻️ fetch data from OWM API on button click
    if the user allowed location, get the latitude and longitude and pass them as a part of the fetch argument which is a call to the OpenWeatherMap API

--- src/App.js
+++ src/App.js
@@ -1,57 +1,51 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Weather from './components/Weather';
 
-// useState & useEffect are Hooks. With Hooks we can use state and other React features without writing a class
+// useState is a Hook. With Hooks we can use state and other React features without writing a class
 
 function App() {
   /* 
-  - 2 states, 1 for the latitude and 1 for the longitude
-  - these 2 states will be used to store the latitude and the longitude of our current location which will be used in the API call to access current weather data
+  - data state is storing all the data from the OpenWeatherAPI call
   */
-  const [lat, setLat] = useState([]);
-  const [lon, setLon] = useState([]);
   const [data, setData] = useState([]);
 
-  // goal of the useEffect function is to load the functions inside it when app loads and reloads
-  useEffect(() => {
-    // async functions always return a promise
-    const fetchData = async () => {
+  const fetchData = () => {
     /* 
     - navigator.geolocation is a property of the Navigator object
     - navigator.geolocation obtains the Geolocation object which is able to obtain the position of the device using the getCurrentPosition method 
     */
-    navigator.geolocation.getCurrentPosition(function(position) {
-      setLat(position.coords.latitude);
-      setLon(position.coords.longitude);
-    });
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+          let lat = position.coords.latitude;
+          let lon = position.coords.longitude;
+          const api = `${process.env.REACT_APP_API_URL}/weather?lat=${lat}&lon=${lon}&units=metric&appid=${process.env.REACT_APP_API_KEY}`;
 
-    /* 
-    - fetch() method is used to fetch resources asynchronously across the network
-    - the simplest use of fetch() takes one argument, the path to the resource we want to fetch and returns a promise containing the response
-    - json() method returns a promise which takes JSON as input and resolves to a JavaScript object
-    - process.env property is used to read environment variables
-    */
-    await fetch(`${process.env.REACT_APP_API_URL}/weather?lat=${lat}&lon=${lon}&units=metric&appid=${process.env.REACT_APP_API_KEY}`)
-          .then(resolve => resolve.json())
-          .then(result => {
-            setData(result);
-            console.log(result);
-          });
-        }
-        // setInterval(() => {
-        //   fetchData();
-        // }, 60000)
-  });
-
-
+              /* 
+      - fetch() method is used to fetch resources asynchronously across the network
+      - the simplest use of fetch() takes one argument, the path to the resource we want to fetch and returns a promise containing the response
+      - json() method returns a promise which takes JSON as input and resolves to a JavaScript object
+      - process.env property is used to read environment variables
+      */
+        fetch(api)
+        .then(resolve => {
+          return resolve.json();
+        })
+        .then(result => {
+          setData(result);
+          console.log(result);
+        })
+      });
+    } 
+  }
 
   return (
     <div className="App">
-    {/* since the return statement is rendered before our API call including the below check will assure that we don't get an error message and show an empty div instead */}
-      {(typeof data.main != 'undefined') ? (
+      <button onClick={fetchData}>Click to get data!</button>
+      {/* since the return statement is rendered before our API call including the below check will assure that we don't get an error message and show an empty div instead */}
+      {(typeof data.main !== 'undefined') ? (
         <Weather weatherData={data} />
       ) : (
-        <div>Data is loading... Just a moment!</div>
+        <div>Please allow location to check your current weather</div>
       )}
 
 
