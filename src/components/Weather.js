d36b46520909523f87feeeb42ca274f2a1bba6b1
Lara Isak <luckasti.larry@gmail.com>
2021-06-22 20:34:58 +0200

    ♻️ simplify context in the Weather component

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import { Card, Image } from 'semantic-ui-react';
 
-const WeatherCard = ({weatherData}) => (
-  <Card>
-    <Card.Content>
-      <Image src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`} />
-      <Card.Header className="city">{weatherData.name}</Card.Header>
-      <Card.Header className="temp">{weatherData.main.temp}</Card.Header>
-    </Card.Content>
-  </Card>
+const WeatherContainer = ({weatherData}) => (
+  <div className="container">
+    <img src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@4x.png`} />
+    <h1>{weatherData.name}</h1>
+    <h2>{weatherData.main.temp}</h2>
+  </div>
 )
 
-export default WeatherCard;
+export default WeatherContainer;
