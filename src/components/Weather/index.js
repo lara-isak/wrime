8c9dc34938ac0e7f30d87bf27f0e83c6c17494f9
Lara Isak <luckasti.larry@gmail.com>
2021-06-23 14:27:18 +0200

    💬 update class name of the weather comp container

--- src/components/Weather/index.js
+++ src/components/Weather/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-const WeatherContainer = ({weatherData}) => (
-  <div className="container">
-    <img src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@4x.png`} />
-    <h1>{weatherData.name}</h1>
-    <h2>{weatherData.main.temp}</h2>
-  </div>
-)
+const WeatherContainer = ({weatherData}) => {
+  return (
+    <div className="weatherContainer">
+      <img src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@4x.png`} />
+      <h1>{weatherData.name}</h1>
+      <h2>{weatherData.main.temp}</h2>
+    </div>
+  )
+};
 
 export default WeatherContainer;
