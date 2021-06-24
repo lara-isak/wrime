a79529aa7ad4e60463f66035a73b4cf9bc34b464
Lara Isak <luckasti.larry@gmail.com>
2021-06-24 17:05:02 +0200

    🩹 add missing alt tag to the weather icon

--- src/components/Weather/index.js
+++ src/components/Weather/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const WeatherContainer = ({weatherData}) => {
   return (
     <div className="weatherContainer">
-      <img src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@4x.png`} />
+      <img src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@4x.png`} alt="Current weather icon" />
       <h1>{weatherData.name}</h1>
       <h2>{weatherData.main.temp}</h2>
     </div>
