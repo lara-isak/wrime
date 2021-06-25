30d82b2c6845826bce0ba334f56064e4f889569e
Lara Isak <luckasti.larry@gmail.com>
2021-06-25 17:01:22 +0200

    🏗️ request access token from Spotify

--- src/components/Playlist/index.js
+++ src/components/Playlist/index.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './style.scss';
 
 const PlaylistContainer = ({weatherData}) => {
+
+  const [token, setToken] = useState('');
+
+  useEffect(() => {
+
+      const api = `${process.env.REACT_APP_SPOTIFY_API_POST_URL }`;
+      const credentials = `${process.env.REACT_APP_SPOTIFY_API_POST_CLIENT_ID}:${process.env.REACT_APP_SPOTIFY_API_POST_CLIENT_SECRET}`;
+
+      axios({
+        url: api,
+        method: 'POST',
+        data: 'grant_type=client_credentials',
+        headers: {
+          'Content-Type' : 'application/x-www-form-urlencoded',
+          /*btoa() method creates a base-64 encoded ASCII string from a "string" of binary data
+            this ensure that data remains intact without modification during transport*/
+          'Authorization' : 'Basic ' + btoa(credentials)
+        }
+      })
+        //then method fires when the above promise action is completed
+        // that is why we can pass a callback function inside .then()
+        // callback function takes the response object from the above link as a parametar
+      .then(
+        (res) => {
+        setToken(res);
+        console.log(token.data.access_token);
+      });
+
+  }, [])
+
   return(
     <div className="playlistContainer">
       <input type="text" name="mood" id="" />
